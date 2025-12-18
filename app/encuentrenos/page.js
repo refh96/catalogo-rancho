eb259fbca6803b72627ab647479eda7381f756a0
refh96 <rodrigo.flores1501@alumnos.ubiobio.cl>
2025-12-18 01:07:48 -0300

    cambiando detalles

--- app/encuentrenos/page.js
+++ app/encuentrenos/page.js
@@ -330,7 +330,7 @@ export default function Encuentrenos() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="text-center">
             <p className="text-gray-400">
-              © 2024 Rancho de Mascotas Hualpén. Todos los derechos reservados.
+              © 2024 Rancho Mascotas Hualpén. Todos los derechos reservados.
             </p>
           </div>
         </div>
